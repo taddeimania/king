21f3ec8ed30c0371b2ddecdd9aaf507e79ea89b5
taddeimania <jtaddei@gmail.com>
2017-04-30 00:31:00 -0400

    Adding things to the adapter for application
    The process of adapting the data coming in from Kong API was fairly
    painless but PATCHing an update to the Kong API ran into a few speed
    bumps.
    
    First off deep in the source of ember there is an aggressive
    `adapter.ajaxOptions` hook that does a JSON.stringify if two conditions
    are met:
    
     1. there is data
     2. it's not a GET request
    
    This posed a problem as the server was expecting www-form-urlencoded
    data and the stringify was adding quotes to the end of my encoded form
    data. So far the only way i've seen to avoid that stringify check is to
    override the ajaxOptions, let it play through, and then go back and
    slice the quotes off.  I'm fairly certain this will cause issues when I
    get to the point where I want to add an API record but we'll cross that
    bridge when we get there.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,28 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
 
-export default DS.RESTAdapter.extend({
+export default DS.JSONAPIAdapter.extend({
   host: ENV.APP.API_HOST,
-  namespace: ""
+  namespace: "",
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+
+  ajaxOptions (url, type, options) {
+    let hash = this._super(...arguments);
+    if (Ember.keys(hash.data).length > 0){
+      hash.data = hash.data.slice(1, -1);
+    }
+    return hash;
+  },
+
+  updateRecord (store, type, snapshot){
+      let id = snapshot.id;
+      if (typeof snapshot._attributes.hosts === "object"){
+        snapshot._attributes.hosts = snapshot._attributes.hosts.join(",");
+      }
+      let data = Ember.$.param(snapshot._attributes);
+      let url = this.buildURL(type.modelName, id, snapshot, 'updateRecord');
+      return this.ajax(url, "PATCH", { data: data });
+  }
 });
