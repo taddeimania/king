e8e838234331c49fbdb255e824f6c1331ca899fe
taddeimania <jtaddei@gmail.com>
2017-04-29 21:22:53 -0400

    Api serializer can handle a single FindRecord.
    Initial spike for retrieving a single api record from kong. Ember Data
    really wants the data to adhere to JSON API spec so if that's what it
    wants that's what we'll give it.

--- app/serializers/api.js
+++ app/serializers/api.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  normalizeFindRecordResponse(store, type, payload) {
+    let newRecord = {data: {}}
+    newRecord.data.attributes = payload;
+    newRecord.data.id = payload.id;
+    return newRecord;
+  },
   normalizeFindAllResponse(store, type, payload) {
     payload.data.forEach((record) => {
       record["type"] = type.modelName
