9534d1c4c4420b2ea0d3895774d865c3fdee61cc
taddeimania <jtaddei@gmail.com>
2017-04-30 23:13:52 -0400

    Added error checking and validation to API Create
    I'm not passing the errors from the serve directly to the page for now,
    instead i'm just taking two use cases of not having required fields and
    displaying an error message if those don't validate. Basic stuff.

--- app/controllers/add-api.js
+++ app/controllers/add-api.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   // defaults
+  error: "",
   stripUri: true,
   preserveHost: false,
   retries: 5,
@@ -10,7 +11,6 @@ export default Ember.Controller.extend({
   upstreamReadTimeout: 60000,
   httpsOnly: false,
   httpIfTerminated: true,
-
   resetFormValues() {
     this.set("apiName", "");
     this.set("apiUpstreamUrl", "");
@@ -25,9 +25,25 @@ export default Ember.Controller.extend({
     this.set("preserveHost", false);
     this.set("httpsOnly", false);
     this.set("httpIfTerminated", true);
+    this.set("error", "");
   },
   actions: {
     createApi() {
+      let apiName = this.get("apiName"),
+          upstream_url = this.get("apiUpstreamUrl"),
+          hosts = this.get("apiHosts"),
+          uris = this.get("apiUris"),
+          methods = this.get("apiMethods");
+      if (!apiName || !upstream_url) {
+        this.set("error", "Cannot create an API without an Api name and an Upstream URL")
+        window.scrollTo(0,0);
+        return ;
+      }
+      if (!hosts && !uris && !methods) {
+        this.set("error", "Need at least ONE of Hosts, Uris, or Methods filled out")
+        window.scrollTo(0,0);
+        return ;
+      }
       let newApi = this.store.createRecord('api', {
         name: this.get("apiName"),
         upstream_url: this.get("apiUpstreamUrl"),
