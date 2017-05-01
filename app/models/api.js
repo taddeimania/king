c1e1e5d090297ce99da266d06b07ea32071b96cd
taddeimania <jtaddei@gmail.com>
2017-04-30 21:56:00 -0400

    Remove created_at for now and fixed typo

--- app/models/api.js
+++ app/models/api.js
@@ -3,10 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   retries: DS.attr(),
-  created_at: DS.attr(),
   http_if_terminated: DS.attr("boolean"),
   https_only: DS.attr("boolean"),
-  preserve_host: DS.attr(),
+  preserve_host: DS.attr("boolean"),
   strip_uri: DS.attr("boolean"),
   upstream_connect_timeout: DS.attr(),
   upstream_read_timeout: DS.attr(),
